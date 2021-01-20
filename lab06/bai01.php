80f2a392cdb76a3902e5ae9a459a7527ce5578cb
TuanAnh <anh.nguyen@idsolutions.com.vn>
2021-01-20 21:34:19 +0700

    lab6 - bai 1

---
+++ lab06/bai01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;
+$y = 7;
+
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+echo $x + $y;
+echo "<br>";
+echo $x - $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x % $y;
+echo "<br>";
+?>
+
+</body>
+</html>
